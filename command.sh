ca372f1b5e63290d879de11affeb3865752384ca
SilverLPs <36015913+SilverLPs@users.noreply.github.com>
2024-12-26 23:08:57 +0100

    command module info output formating adjustment

--- command.sh
+++ command.sh
@@ -11,12 +11,12 @@ done
 
 echo "MODULE INFO:"
 echo
-echo "Name:      command"
-echo "Package:   bisonbackup.general"
-echo "Path:      $(pwd)"
-echo "Shell:     $SHELL"
-echo "Time:      $(date +\%Y\%m\%d)-$(date +\%H\%M\%S)"
-echo "User:      $(whoami)"
-echo "COMMAND:   $VAR_COMMAND"
+echo "Name:        command"
+echo "Package:     bisonbackup.general"
+echo "Path:        $(pwd)"
+echo "Shell:       $SHELL"
+echo "Time:        $(date +\%Y\%m\%d)-$(date +\%H\%M\%S)"
+echo "User:        $(whoami)"
+echo "COMMAND:     $VAR_COMMAND"
 echo
 $VAR_COMMAND
